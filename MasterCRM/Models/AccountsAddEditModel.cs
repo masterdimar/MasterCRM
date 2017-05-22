3c31087dd2a4870e636677583ab40fc4bfbaf52d
Pablo Di Martino <masterdimar@yahoo.com.ar>
2017-05-22 19:45:51 -0300

    modal

---
+++ MasterCRM/Models/AccountsAddEditModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MasterCRM.Models
+{
+    public class AccountsAddEditModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int OwnerID { get; set; }
+        public int SectorID { get; set; }
+        public int TipoID { get; set; }
+        public string Web { get; set; }
+    }
+}
